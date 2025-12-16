e25aa1a4a981a19393d94ad1489dd1c8ad6ada94
JacobCarriere <jacob2000@live.com>
2025-12-15 21:25:24 -0500

    Updated placeholder movies

--- FinalProject/app/src/main/java/com/example/finalproject/ui/screens/movies/MoviesScreen.kt
+++ FinalProject/app/src/main/java/com/example/finalproject/ui/screens/movies/MoviesScreen.kt
@@ -24,10 +24,14 @@ fun MoviesScreen(navController: NavController, appViewModel: AppViewModel) {
     // Placeholder movie list
     val movies = remember {
         mutableStateListOf(
-            Movie("1", "Batman Begins", "2005", "https://via.placeholder.com/150"),
-            Movie("2", "The Dark Knight", "2008", "https://via.placeholder.com/150"),
-            Movie("3", "The Dark Knight Rises", "2012", "https://via.placeholder.com/150")
-        )
+            Movie("1", "Heathers", "1989", "https://a.ltrbxd.com/resized/film-poster/5/0/1/7/7/50177-heathers-0-2000-0-3000-crop.jpg?v=dbd1f7062c"),
+            Movie("2", "Scott Pilgrim vs. the World", "2010", "https://a.ltrbxd.com/resized/sm/upload/vs/75/02/fx/2B5zjs5E3xerqAyowpw3QcOCyLq-0-2000-0-3000-crop.jpg?v=3aef2095df"),
+            Movie("3", "Whiplash", "2014", "https://a.ltrbxd.com/resized/sm/upload/cl/dn/kr/f1/4C9LHDxMsoYI0S3iMPZdm3Oevwo-0-2000-0-3000-crop.jpg?v=d13ea36528"),
+            Movie("4", "La La Land", "2016", "https://a.ltrbxd.com/resized/film-poster/2/4/0/3/4/4/240344-la-la-land-0-2000-0-3000-crop.jpg?v=053670ff84"),
+            Movie("5", "The Nice Guys", "2016", "https://a.ltrbxd.com/resized/film-poster/2/1/6/3/0/1/216301-the-nice-guys-0-2000-0-3000-crop.jpg?v=40616daa2c"),
+            Movie("6", "Your Name.", "2016", "https://a.ltrbxd.com/resized/sm/upload/md/ku/l7/m5/xq1Ugd62d23K2knRUx6xxuALTZB-0-2000-0-3000-crop.jpg?v=443be7928f"),
+            Movie("7", "Knives Out", "2017", "https://a.ltrbxd.com/resized/film-poster/4/7/5/3/7/0/475370-knives-out-0-2000-0-3000-crop.jpg?v=7da76d742c"),
+            )
     }
 
     Column(
